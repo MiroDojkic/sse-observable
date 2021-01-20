2b491123e8ed9b9140724e2612e7b1a17dabd408
Miro Dojkic <mdojkic@gmail.com>
2021-01-20 01:54:28 +0100

    Reestablish connection ✨

--- src/index.js
+++ src/index.js
@@ -4,34 +4,46 @@ import { ReadableWebToNodeStream } from 'readable-web-to-node-stream';
 import split from 'split2';
 import to from 'to2';
 
+const states = {
+  CONNECTING: 'CONNECTING',
+  OPEN: 'OPEN',
+  CLOSED: 'CLOSED'
+};
+const DEFAULT_RECONNECTION_DELAY = 30000;
+
 function createObservable(path, opts) {
   const emitter = mitt();
+  let readyState = states.CONNECTING;
+  let reconnectDelay = DEFAULT_RECONNECTION_DELAY;
   let lastEventId;
   const controller = new AbortController();
   const { signal } = controller;
-  signal.addEventListener('abort', removeListeners);
-  connect(path, { ...opts, signal });
-  return { ...emitter, close };
+  const headers = new Headers(opts.headers);
+  const request = new Request(path);
+  emitter.on('error', removeListeners);
+  connect();
+  return { ...emitter, close, readyState, url: request.url };
 
   function close() {
     controller.abort();
+    readyState = states.CLOSED;
+    emitter.emit('error');
   }
 
-  function connect(path, opts) {
-    return window.fetch(path, opts)
+  function connect() {
+    if (lastEventId) headers.set('Last-Event-ID', lastEventId);
+    return window.fetch(request, { ...opts, headers, signal })
       .then(response => {
-        if (!response.ok) {
-          emitter.emit('error');
-          throw new Error(`Failed to connect to SSE endpoint: ${path}`);
-        }
+        if (!response.ok || response.status === 204) return close();
+        readyState = states.OPEN;
         emitter.emit('open');
         return pump(
           new ReadableWebToNodeStream(response.body),
           split('\n\n'),
           to(emitEvent),
-          removeListeners
+          retryConnect
         );
-      })
+      });
   }
 
   function emitEvent(buf, enc, next) {
@@ -50,9 +62,16 @@ function createObservable(path, opts) {
       const [, name, value] = field;
       if (name === 'data') data.push(value);
       if (!value) return;
+      if (name === 'retry') reconnectDelay = value;
       if (name === 'event') { event = value; }
       if (name === 'id') { lastEventId = value; }
     }
+  }
+
+  function retryConnect(error) {
+    if (readyState !== states.OPEN) return close();
+    readyState = states.CONNECTING;
+    return setTimeout(connect, reconnectDelay);
   }
 
   function removeListeners(error) {
